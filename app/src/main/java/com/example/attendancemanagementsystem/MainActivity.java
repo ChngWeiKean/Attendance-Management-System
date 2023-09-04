59d38c65056709ba524f2a41032d96891881caa7
ChngWeiKean <129861018+ChngWeiKean@users.noreply.github.com>
2023-09-04 20:49:13 +0800

    login UI
    Login page without backend, nothing on authentication yet, just UI

--- app/src/main/java/com/example/attendancemanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/attendancemanagementsystem/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.attendancemanagementsystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+    Button loginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView signup = (TextView) findViewById(R.id.signupText);
+        signup.setMovementMethod(LinkMovementMethod.getInstance());
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (email.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
